c8c477a688d89e582c4c7188f2cdf8aad6b9f873
tharun <ch.tharunkumar1@gmail>
2020-10-12 12:20:13 +0530

    ver 1

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+
+$(".cb").on("click",function(){
+  var col=$("#nc").val();
+$(".navbar").css("background-color",col);
+});
+
+$(".cb1").on("click",function(){
+  var col=$("#cc").val();
+$(".contact").css("background-color",col);
+});
